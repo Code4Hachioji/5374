e8461e9c8d445f208e9d2ca4ccea4ce71d00937f
ansurinfo <magics@ansur.info>
2015-08-13 19:25:33 +0900

    2015first

---
+++ js/setting.js
@@ -0,0 +1,14 @@
+/*
+ * 5374 setting
+ *
+*/
+
+var SVGLabel = true; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+
+var MaxDescription = 12; // ごみの最大種類、９を超えない場合は変更の必要はありません。
+
+var MaxMonth = 3;
+
+var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+
+var SkipSuspend = true; // 休止期間を除去するときは、true。奈良の仕様は、true。
